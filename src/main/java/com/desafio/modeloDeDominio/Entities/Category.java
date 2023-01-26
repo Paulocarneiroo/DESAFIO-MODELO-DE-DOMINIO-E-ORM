8d311bac3f51f7e24ac5183f34e664896a965bbd
Paulo Carneiro <paulocarneirosegundo@gmail.com>
2023-01-25 21:25:35 -0300

    first commit Modelo-de-Dominio-e-ORM

---
+++ src/main/java/com/desafio/modeloDeDominio/Entities/Category.java
@@ -0,0 +1,48 @@
+package com.desafio.modeloDeDominio.Entities;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+
+    @OneToMany(mappedBy = "category")
+    private Set<Activity> activities = new HashSet<>();
+
+    public Category(Integer id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Activity> getActivities(){
+        return activities
+                .stream()
+                .toList();
+    }
+}
---
+++ src/test/java/com/desafio/modeloDeDominio/ModeloDeDominioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.modeloDeDominio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModeloDeDominioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
